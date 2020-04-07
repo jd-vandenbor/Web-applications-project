3064669bd5d099050c3ce53882172ea905eb0f87
sriddel <36021975+sriddel@users.noreply.github.com>
2020-04-07 14:15:51 -0400

    insert DB with new attractions

--- project/insertdb.php
+++ project/insertdb.php
@@ -1,4 +1,3 @@
-<?php
 $servername = "localhost";
 $username = "name";
 $password = "cps630cps";
@@ -12,14 +11,21 @@ try {
 catch (PDOException $e){
 	echo "Connection failed: " . $e->getMessage();
 }
-//insert student into data base
+//insert into data base
 try{
 	$name =$_POST['name'];
 	$country =$_POST['country'];
 	$continent =$_POST['continent'];
-	$insertsql = "INSERT INTO attractions (name, country, continent) VALUES ('$name', '$country', '$continent')";
+	$price =$_POST['price'];
+	$lat =$_POST['lat'];
+	$longti =$_POST['longti'];
+	$address =$_POST['address'];
+	$description =$_POST['description'];
+	$image =$_POST['imagefile'];
+	$pagelink =$_POST['pagelink'];
+	$insertsql = "INSERT INTO attractions (name, country, continent, price, lat, longti, address, description, image, link ) VALUES ('$name', '$country', '$continent', '$price', '$lat', '$longti', '$address', '$description', '$image', '$pagelink')";
 	$conn->query($insertsql);
-	echo "Inserted '$name' successfully";
+	echo "Inserted '$name' successfully $insertsql";
 }
 catch (PDOException $dbie){
 	echo "insert failed". $dbie->getMessage();
