ec340fbbcfbf4a4f7baea1c879d89db3f5fd4915
sriddel <36021975+sriddel@users.noreply.github.com>
2020-04-07 14:10:23 -0400

    added sign up page

---
+++ project/signup.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>Make an Account</title>
+	<meta http-equiv="Content-Type" content="text/html; charset = UTF-8">
+</head>
+
+<body>
+	 <form action="register.php" method="POST">
+
+        Full Name:<input type="text" name="name" id="name">
+		<br>
+		Username:<input type="text" name="username" id="username">
+		<br>
+		Password:<input type="text" name="password" id="password">
+		<br>
+		Address:<input type="text" name="address" id="address">
+		<br>
+		Email:<input type="text" name="email" id="email">
+		<br>
+		Phone Number<input type="text" name="phonenum" id="phonenum">
+        <br>
+
+        <input type="submit">
+                       
+     </form>
+	
+	
+	<br>
+	<a href="home.php">back</a>
+</body>
+
+</html>
