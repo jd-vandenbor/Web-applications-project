6eae96da8b443ce781995a5f36ca7c98ec7c59da
sriddel <36021975+sriddel@users.noreply.github.com>
2020-04-20 19:39:23 -0400

    Update advsearch.php

--- project/advsearch.php
+++ project/advsearch.php
@@ -1,11 +1,43 @@
 <html>
+<style>
+
+
+.divy {
+	background-color: #f2efe1;
+}
+
+input[type=text], select {
+	width: 30%;
+	padding: 12px 20px;
+	margin: 8px 0;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	box-sizing: border-box;
+}
+
+input[type=submit] {
+	color: white;
+  	background-color: #75aaff;
+  	padding: 14px 20px;
+  	margin: 8px 0;
+  	border: none;
+  	border-radius: 4px;
+  	cursor: pointer;
+}
+
+input[type=submit]:hover {
+  	background-color: #2e4c7d;
+}
+
+</style>
 <head>
 	<title>review test</title>
 	<meta http-equiv="Content-Type" content="text/html; charset = UTF-8">
 </head>
 
 <body>
-
+<h1>Please input your search details below</h1>
+<div class="divy" style="padding-top:20px;padding-bottom:150px;">
 <form action="search.php" method="POST">
 
         Name:<input type="text" name="name" id="name">
@@ -30,5 +62,6 @@
 </form>
 <br>
 <a href="home.php">back</a>
+</div>
 </body>
 </html>
