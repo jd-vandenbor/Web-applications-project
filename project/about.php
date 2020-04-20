f66ae0154e57e0b29f575fcfa15df87b32f08caa
sriddel <36021975+sriddel@users.noreply.github.com>
2020-04-20 19:41:14 -0400

    Create about.php

---
+++ project/about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="./home.css">
+<style>
+
+</style>
+</head>
+<body>
+  Joshua Vandenbor: joshua.vandenbor@ryerson.ca,
+  Sean Ridell: sean.ridell@ryerson.ca,
+  Trevor Jarvis: trevor.jarvis@ryerson.ca,
+  
+
+</body>
+
+</html>
