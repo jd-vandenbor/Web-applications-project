36c4dbe02b8e59f1fd354e13fc42d6288bc694fd
sriddel <36021975+sriddel@users.noreply.github.com>
2020-04-20 20:42:44 -0400

    added reviews

--- project/readmore.php
+++ project/readmore.php
@@ -151,5 +151,59 @@ The CN Tower held the record for the world's tallest free-standing structure for
     description.innerHTML = descrip;
     }
 </script>
+<br>
+<a href="home.php">back</a>
+<?php
 
+$servername = "localhost";
+$username = "name";
+$password = "cps630cps";
+$dbname = "places";
+//start MYSQL
+try {
+	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	//echo "Connected";
+}
+catch (PDOException $e){
+	echo "Connection failed: " . $e->getMessage();
+}
+$_SESSION['currentattraction'] = $_GET['attraction'];
+if(isset($_SESSION['user'])){
+	echo "<form action=\"addreview.php\" id=\"attr\" method=\"POST\">";
+	echo"<label for=\"rating\">Your Rating:</label>
+		<select id=\"rating\" name=\"rating\">
+			<option value=\"0\">0</option>
+			<option value=\"1\">1</option>
+			<option value=\"2\">2<option>
+			<option value=\"3\">3</option>
+			<option value=\"4\">4</option>
+			<option value=\"5\">5</option>
+		</select>          
+		<br>";
+		echo"<textarea id=\"reviewtext\" name=\"reviewtext\" form=\"attr\" rows=\"4\" cols=\"50\">
+Enter description here
+		</textarea>
+		<input type=\"hidden\" name=\"form_submitted\" value=\"1\" />
+
+        <input type=\"submit\" value=\"submit\">
+                       
+     </form>";
+}else{echo "Login to leave a review!";}
+try {
+	$selectsql = "SELECT * FROM reviews WHERE attractionname = '".$_SESSION['currentattraction']."' ";
+	$result = $conn->query($selectsql);
+
+	echo "<table><tr><th>User Name</th><th>Rating</th><th>Review</th></tr>";
+
+	while($row = $result->fetch()) {
+		echo "<tr><td>".$row['username']."</td><td>".$row['rating']." ".$row['reviewtext']."</td></tr>";
+	}
+	echo "</table>";
+
+}
+catch (PDOException $dbse){
+	echo "Select failed". $dbse->getMessage();
+}
+?>
 </html>
