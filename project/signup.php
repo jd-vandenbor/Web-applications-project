f3f1a457a5e062c3e600dc043aa68b7e08518930
Josh Vandenbor <jd.vandenbor@gmail.com>
2020-04-20 20:18:28 -0400

    css to signup

--- project/signup.php
+++ project/signup.php
@@ -3,7 +3,39 @@
 	<title>Make an Account</title>
 	<meta http-equiv="Content-Type" content="text/html; charset = UTF-8">
 </head>
+<style>
 
+
+.divy {
+	background-color: #f2efe1;
+}
+
+input[type=text], select {
+	width: 30%;
+	padding: 12px 20px;
+	margin: 8px 0;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	box-sizing: border-box;
+}
+
+input[type=submit] {
+	color: white;
+  	background-color: #75aaff;
+  	padding: 14px 20px;
+  	margin: 8px 0;
+  	border: none;
+  	border-radius: 4px;
+  	cursor: pointer;
+}
+
+input[type=submit]:hover {
+  	background-color: #2e4c7d;
+}
+
+</style>
+<h1>Please input the user credentials below and hit submit</h1>
+<div class="divy" style="padding-top:20px;padding-bottom:150px;">
 <body>
 	 <form action="register.php" method="POST">
 
@@ -27,6 +59,7 @@
 	
 	<br>
 	<a href="home.php">back</a>
+	</div>
 </body>
 
 </html>
